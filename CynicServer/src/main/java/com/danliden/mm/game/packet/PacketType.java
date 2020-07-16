0e27647bd19a9680385c56c51d6359bcac4f2159
Danelido <AlexanderAlsvold@hotmail.se>
2020-07-16 22:05:13 +0200

    Preparing for lobby system implementation.

--- CynicServer/src/main/java/com/danliden/mm/game/packet/PacketType.java
+++ CynicServer/src/main/java/com/danliden/mm/game/packet/PacketType.java
@@ -7,6 +7,9 @@ public abstract class PacketType {
         public static final int LEFT_SESSION = 1;
         public static final int HEARTBEAT = 2;
         public static final int CLIENT_UPDATE = 3;
+        public static final int VOTE_TO_START_SESSION = 4;
+        public static final int REMOVE_VOTE_TO_START_SESSION = 5;
+        public static final int SETUP_UPDATE = 6;
     }
 
     public abstract static class Outgoing {
@@ -17,6 +20,9 @@ public abstract class PacketType {
         public static final int UPDATED_CLIENT = 3;
         public static final int DECLINED_JOIN_REQUEST = 4;
         public static final int NEW_PLAYER_JOINED = 5;
+        public static final int LOBBY_UPDATE = 6;
+        public static final int UPDATED_SETUP = 7;
+        public static final int STARTING_GAME = 8;
     }
 
 }
