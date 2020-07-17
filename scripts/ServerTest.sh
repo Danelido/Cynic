4e7255f6e48eff60920d04d0ec0b388110b4deb2
Danelido <AlexanderAlsvold@hotmail.se>
2020-07-17 23:42:43 +0200

    Remove process killing on jenkins due to not permitted.

--- scripts/ServerTest.sh
+++ scripts/ServerTest.sh
@@ -2,9 +2,3 @@ echo "Running Cynic Server..."
 java -jar CynicServer/target/CynicServer-1.0.0.jar &
 echo "Running Cynic Server Test"
 java -jar CynicServerTest/target/CynicServerTest-1.0.0.jar "139.162.149.158" "13500"
-
-if apid=$(pgrep -f CynicServer/target/CynicServer-1.0.0.jar)
-then
-    echo "Killing server, pid was $apid"
-    kill -9 $apid
-fi
